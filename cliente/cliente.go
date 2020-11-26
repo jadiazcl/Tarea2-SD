d8faef6fefff954cbd5df059fb5087f181c411f0
jorge <jorge.diazma@sansano.usm.cl>
2020-11-26 00:55:27 -0300

    a

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -11,8 +11,10 @@ import (
     pb"Lab2/Tarea2-SD/pipeline"
 )
 
+// maquinas=["158,159,160"]
 
-func enviar_ordenes( delta_tiempo float64){
+//func enviar_ordenes( id_maquina  int, parte int){
+func enviar_ordenes(){
   var conn *grpc.ClientConn
   conn, err := grpc.Dial("dist160:50054", grpc.WithInsecure())
   if err != nil {
@@ -29,10 +31,23 @@ func enviar_ordenes( delta_tiempo float64){
       log.Fatalf("Error when calling SayHello: %s", err)
     }
     log.Printf("El codigo de seguimiento del pedido es: %d", response.Valor)
+    //fileName := "bigfile_" + strconv.FormatUint(parte, 10)
     fileName := "bigfile_" + strconv.FormatUint(1, 10)
     ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
   }
 }
+
+// func recuperar_archivo(){
+//     enviar_ordenes( 0,0)
+//     enviar_ordenes( 0,1)
+//     enviar_ordenes( 1,2)
+//     enviar_ordenes( 2,3)
+//
+//
+//   //rearmar objeto
+// }
+
+
 func main() {
 	enviar_ordenes(2)
 
