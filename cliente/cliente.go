92780bd9f6b2365bfa40309eaa8c5e306f0390a1
jorgeSamurSansur <jorge.samur@sansano.usm.cl>
2020-11-26 15:57:14 -0300

    more changes cliente.go /sammy

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "log"
-  "fmt"
-  "io/ioutil"
-  "os"
-  "strconv"
-  "golang.org/x/net/context"
-  "google.golang.org/grpc"
-  pb"Lab2/Tarea2-SD/pipeline"
+	pb "Lab2/Tarea2-SD/pipeline"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
-// func request_chunk(){
+// func requestChunk(){
 //   var conn *grpc.ClientConn
 //   conn, err := grpc.Dial("dist160:50054", grpc.WithInsecure())
 //   if err != nil {
@@ -34,47 +35,48 @@ import (
 //   }
 // }
 
-var machines string[] = ["dist157", "dist158", "dist159", "dist160"]
+var machines [4]string
 
+//func requestChunk( idMchn  int, parte int){
+func requestChunk(idMchn int) {
 
-//func request_chunk( id_maquina  int, parte int){
-func request_chunk(id_maquina  int,){
-  var conn *grpc.ClientConn
-  mchn := machines[id_maquina]
-  conn, err := grpc.Dial( mchn + ":50054", grpc.WithInsecure())
-  if err != nil {
-    log.Fatalf("did not connect: %s", err)
-  }
-  opcion:=0
-  defer conn.Close()
-  for opcion!=-1{
-      fmt.Println("waiting >>>")
-      fmt.Scanf("%d", &opcion)
-    c := pb.NewGreeterClient(conn)
-   // response, err := c.SayHello(context.Background(), &pb.Test{Valor:int32(parte), Chuck:------}
-    response, err := c.SayHello(context.Background(), &pb.Solcamion{IdCamion:int32(opcion)})
+	machines = []string{"dist157", "dist158", "dist159", "dist160"}
+	var conn *grpc.ClientConn
+	mchn := machines[idMchn]
+	conn, err := grpc.Dial(mchn+":50054", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	opcion := 0
+	defer conn.Close()
+	for opcion != -1 {
+		fmt.Println("waiting >>>")
+		fmt.Scanf("%d", &opcion)
+		c := pb.NewGreeterClient(conn)
+		// response, err := c.SayHello(context.Background(), &pb.Test{Valor:int32(parte), Chuck:------}
+		response, err := c.SayHello(context.Background(), &pb.Solcamion{IdCamion: int32(opcion)})
 
-    if err != nil {
-      log.Fatalf("Error when calling SayHello: %s", err)
-    }
-    log.Printf("El codigo de seguimiento del pedido es: %d", response.Valor)
-    fileName := "bigfile_" + strconv.FormatUint(parte, 10)
-    // fileName := "bigfile_" + strconv.FormatUint(opcion, 10)
-    ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
-  }
+		if err != nil {
+			log.Fatalf("Error when calling SayHello: %s", err)
+		}
+		log.Printf("El codigo de seguimiento del pedido es: %d", response.Valor)
+		fileName := "bigfile_" + strconv.FormatUint(parte, 10)
+		// fileName := "bigfile_" + strconv.FormatUint(opcion, 10)
+		ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
+	}
 }
+
 // func recuperar_archivo(){
-//     request_chunk( 0,0)
-//     request_chunk( 0,1)
-//     request_chunk( 1,2)
-//     request_chunk( 2,3)
+//     requestChunk( 0,0)
+//     requestChunk( 0,1)
+//     requestChunk( 1,2)
+//     requestChunk( 2,3)
 //
 //
 //   //rearmar objeto
 // }
 
-
 func main() {
-	request_chunk(3)
+	requestChunk(3)
 
 }
