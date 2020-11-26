185a6507658165b8ce2281b1b14c54e5e4e87faf
jorgeSamurSansur <jorge.samur@sansano.usm.cl>
2020-11-26 15:35:17 -0300

    Cliente Cambios

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -8,14 +8,39 @@ import (
   "strconv"
   "golang.org/x/net/context"
   "google.golang.org/grpc"
-    pb"Lab2/Tarea2-SD/pipeline"
+  pb"Lab2/Tarea2-SD/pipeline"
 )
 
-// maquinas=["158,159,160"]
+// func request_chunk(){
+//   var conn *grpc.ClientConn
+//   conn, err := grpc.Dial("dist160:50054", grpc.WithInsecure())
+//   if err != nil {
+//     log.Fatalf("did not connect: %s", err)
+//   }
+//   opcion:=0
+//   defer conn.Close()
+//   for opcion!=-1{
+//       fmt.Println("Ingrese -1 para cerrar el programa ")
+//       fmt.Scanf("%d", &opcion)
+//     c := pb.NewGreeterClient(conn)
+//     response, err := c.SayHello(context.Background(), &pb.Solcamion{IdCamion:int32(opcion)})
+//     if err != nil {
+//       log.Fatalf("Error when calling SayHello: %s", err)
+//     }
+//     log.Printf("El codigo de seguimiento del pedido es: %d", response.Valor)
+//     //fileName := "bigfile_" + strconv.FormatUint(parte, 10)
+//     fileName := "bigfile_" + strconv.FormatUint(1, 10)
+//     ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
+//   }
+// }
 
-//func enviar_ordenes( id_maquina  int, parte int){
-func enviar_ordenes(){
+
+var machines string[] = ["dist158", "dist159", "dist160"]
+
+
+func request_chunk( id_maquina  int, parte int){
   var conn *grpc.ClientConn
+  mchn := 
   conn, err := grpc.Dial("dist160:50054", grpc.WithInsecure())
   if err != nil {
     log.Fatalf("did not connect: %s", err)
@@ -23,25 +48,24 @@ func enviar_ordenes(){
   opcion:=0
   defer conn.Close()
   for opcion!=-1{
-      fmt.Println("Ingrese -1 para cerrar el programa ")
+      fmt.Println("waiting >>>")
       fmt.Scanf("%d", &opcion)
     c := pb.NewGreeterClient(conn)
-    response, err := c.SayHello(context.Background(), &pb.Solcamion{IdCamion:int32(opcion)})
+    response, err := c.SayHello(context.Background(), &pb.Test{Valor:int32(parte), Chuck:------})
     if err != nil {
       log.Fatalf("Error when calling SayHello: %s", err)
     }
     log.Printf("El codigo de seguimiento del pedido es: %d", response.Valor)
     //fileName := "bigfile_" + strconv.FormatUint(parte, 10)
-    fileName := "bigfile_" + strconv.FormatUint(1, 10)
+    fileName := "bigfile_" + strconv.FormatUint(opcion, 10)
     ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
   }
 }
-
 // func recuperar_archivo(){
-//     enviar_ordenes( 0,0)
-//     enviar_ordenes( 0,1)
-//     enviar_ordenes( 1,2)
-//     enviar_ordenes( 2,3)
+//     request_chunk( 0,0)
+//     request_chunk( 0,1)
+//     request_chunk( 1,2)
+//     request_chunk( 2,3)
 //
 //
 //   //rearmar objeto
@@ -49,6 +73,6 @@ func enviar_ordenes(){
 
 
 func main() {
-	enviar_ordenes()
+	request_chunk()
 
 }
