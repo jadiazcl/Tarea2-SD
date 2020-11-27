855acb8cc513012d082c0f0b1b2e131da6334ef8
jorgeSamurSansur <jorge.samur@sansano.usm.cl>
2020-11-27 19:19:11 -0300

    trying to stitch the file /sammy

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	pb "Lab2/Tarea2-SD/pipeline"
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,30 +13,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-// func requestChunk(idMchn int) {
-
-// 	machines := []string{"dist157", "dist158", "dist159", "dist160"}
-// 	var conn *grpc.ClientConn
-// 	mchn := machines[idMchn]
-// 	conn, err := grpc.Dial(mchn+":50054", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatalf("did not connect: %s", err)
-// 	}
-// 	fileChunk := 0
-// 	defer conn.Close()
-// 	fmt.Println("waiting >>>")
-// 	fmt.Scanf("%d", &fileChunk)
-// 	c := pb.NewGreeterClient(conn)
-// 	response, err := c.SayHello(context.Background(), &pb.Solcamion{IdCamion: int32(fileChunk)})
-
-// 	if err != nil {
-// 		log.Fatalf("Error when calling SayHello: %s", err)
-// 	}
-// 	log.Printf("La parte solicitada es: %d", response.Valor)
-// 	fileName := "bigfile_" + strconv.FormatUint(uint64(fileChunk), 10)
-// 	ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
-// 	//REARMAR
-// }
 func requestChunk(idMchn int) {
 
 	machines := []string{"dist157", "dist158", "dist159", "dist160"}
@@ -49,7 +26,7 @@ func requestChunk(idMchn int) {
 	fileChunk := 0
 	bookTag := ""
 	defer conn.Close()
-
+	// Esto debe ser cambiado para poder recibir todo desde un json o txt
 	fmt.Println("waiting >>>")
 	fmt.Println("*     Chunk Solicitado      *")
 	fmt.Scanf("%d", &fileChunk)
@@ -68,14 +45,68 @@ func requestChunk(idMchn int) {
 	log.Printf("La parte solicitada es: %d", response.Valor)
 	fileName := bookTag + "_" + strconv.FormatUint(uint64(fileChunk), 10)
 	ioutil.WriteFile(fileName, response.Chuck, os.ModeAppend)
-	//REARMAR
+
+}
+
+var writePosition int64 = 0
+
+func stitchTheFile(originalName string, totalPartsNum uint64) {
+
+	for j := uint64(0); j < totalPartsNum; j++ {
+		currentChunkFileName := originalName + "_" + strconv.FormatUint(j, 10)
+
+		newFileChunk, err := os.Open(currentChunkFileName)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		defer newFileChunk.Close()
+
+		chunkInfo, err := newFileChunk.Stat()
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		chunkSize := chunkInfo.Size()
+		chunkBufferBytes := make([]byte, chunkSize)
+
+		fmt.Println("Appending at position : [", writePosition, "] bytes")
+		writePosition = writePosition + chunkSize
+
+		reader := bufio.NewReader(newFileChunk)
+		_, err = reader.Read(chunkBufferBytes)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		n, err := file.Write(chunkBufferBytes)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		file.Sync()
+		chunkBufferBytes = nil
+
+		fmt.Println("Written ", n, " bytes")
+
+		fmt.Println("Recombining part [", j, "] into : ", newFileName)
+	}
+	file.Close()
 }
 
 func main() {
 	requestChunk(1)
-	requestChunk(2)
-	requestChunk(3)
 	requestChunk(1)
-	requestChunk(2)
+	requestChunk(1)
+	requestChunk(1)
+	requestChunk(1)
+
+	stitchTheFile("test.pdf", 5)
 
 }
