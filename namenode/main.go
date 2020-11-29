73e34cf1c3fea94cc6a92fe3a68e2c862aee2bc1
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-11-29 01:35:50 -0300

    test buscar en log

--- namenode/main.go
+++ namenode/main.go
@@ -2,70 +2,82 @@ package main
 
  import (
          "fmt"
-         //"log"
-         //"net"
+         "log"
+         "os"
+         "strings"
+         "strconv"
+         "net"
          "io/ioutil"
-         //"google.golang.org/grpc"
-         //"context"
-          //pb"Lab2-Test/Tarea2-SD/pipeline"
+         "bufio"
+         "google.golang.org/grpc"
+         "context"
+          pb"Lab2-Test/Tarea2-SD/pipeline"
  )
- // Maquina{ 1: 158 ; 2 : 159 ; 3 : 160}
-
-
 
  var Maquinas = map[string]int{"dist157": 0, "dist158": 1,"dist159": 2,"dist160": 3}
 
 
- //type Server struct {
-   //  pb.UnimplementedGreeterServer
- //}
+ type Server struct {
+    pb.UnimplementedGreeterServer
+ }
 
- //La funcion GRPC para la consulta de la ubicacion del archivo
- //func (s *Server) SolicitarUbicaciones(ctx context.Context, in *pb.ConsultaUbicacion) (*pb.RespuestaUbicacion, error) {
-  //  log.Printf("recibi %s ", in.NombreArchivo )
-   // partes,ubicaciones:=ubicacion_archivo(int(in.IdArchivo))
- 	//return &pb.RespuestaUbicacion{Partes: int32(partes),Ubicaciones:ubicaciones}, nil
- //}
-
- func buscar_en_log(){
-    b, err := ioutil.ReadFile("log.txt") // just pass the file name
-    if err != nil {
-    fmt.Print(err)
-    }
-    
-
-    str := string(b) // convert content to a 'string'
-
-    fmt.Println(str) // print the content as a 'string'
+ La funcion GRPC para la consulta de la ubicacion del archivo
+ func (s *Server) SolicitarUbicaciones(ctx context.Context, in *pb.ConsultaUbicacion) (*pb.RespuestaUbicacion, error) {
+   log.Printf("recibi %s ", in.NombreArchivo )
+   partes,ubicaciones:=buscar_en_log(in.NombreArchivo)
+ 	return &pb.RespuestaUbicacion{Partes: int32(partes),Ubicaciones:ubicaciones}, nil
  }
 
  // Esta funcion busca la ubicacion de las partes del archivo
- func ubicacion_archivo(id_archivo int) (int, string){
-    // Codigo para buscar el archivo
-    ubicacion:="1--1-1-1"
-    return 5,ubicacion
-
+ func buscar_en_log(nombre_libro string) (int, string){
+    file, err := os.Open("log.txt")
+    if err != nil {
+        log.Fatalf("Error when opening file: %s", err)
+    }
+    fileScanner := bufio.NewScanner(file)
+    ubicacion:=""
+    cantidad_saltos:=0
+    for fileScanner.Scan() {
+      if (cantidad_saltos==0){
+        linea:=fileScanner.Text()
+        partes:=strings.Split(linea, " ")
+        nombre_registro:=partes[0]
+        cantidad_saltos,err=strconv.Atoi(partes[1])
+        if (nombre_registro==nombre_libro){
+          for index := 0; index < cantidad_saltos; index++ {
+            fileScanner.Scan()
+            linea:=fileScanner.Text()
+            partes:=strings.Split(linea, " ")
+            ubicacion=ubicacion+partes[1]+"-"
+          }
+          break
+        }
+      }else{
+        cantidad_saltos=cantidad_saltos-1
+      }
+    }
+    return cantidad_saltos,ubicacion
  }
 
+
  //funcion para recepcionar conexiones
- //func  recepcion_clientes(){
-  // lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 50054))
-   //if err != nil {
-    // log.Fatalf("failed to listen: %v", err)
-  // }
-   //grpcServer := grpc.NewServer()
+ func  recepcion_clientes(){
+  lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 50054))
+   if err != nil {
+    log.Fatalf("failed to listen: %v", err)
+  }
+   grpcServer := grpc.NewServer()
 
-   //pb.RegisterGreeterServer(grpcServer, &Server{})
+   pb.RegisterGreeterServer(grpcServer, &Server{})
 
-   //if err := grpcServer.Serve(lis); err != nil {
-    // log.Fatalf("failed to serve: %s", err)
-  // }
-// }
- 
+   if err := grpcServer.Serve(lis); err != nil {
+    log.Fatalf("failed to serve: %s", err)
+  }
+}
+
  func main() {
-    
-    //go recepcion_clientes()
-    buscar_en_log()
+
+    go recepcion_clientes()
     opcion:=0
     for opcion!=-1{
         fmt.Println("Ingrese -1 para cerrar el programa ")
