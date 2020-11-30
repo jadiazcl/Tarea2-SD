9ae33557b4a4600b667dd97dfb697fdb639a26ee
jorgeSamurSansur <jorge.samur@sansano.usm.cl>
2020-11-30 18:00:24 -0300

    Please, work

--- datanode/main.go
+++ datanode/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	pb "Lab2-Test/Tarea2-SD/pipeline"
+	"context"
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"log"
 	"math"
+	"math/rand"
 	"net"
 	"os"
-	"context"
+	"strconv"
+	"time"
+
 	"google.golang.org/grpc"
-	pb "Lab2-Test/Tarea2-SD/pipeline"
 )
 
 type Server struct {
@@ -18,13 +21,14 @@ type Server struct {
 }
 
 /*-----------------------------------------------------------------------------------------*/
-
 func (s *Server) SayHello(ctx context.Context, in *pb.Book) (*pb.Test, error) {
 	req := int(in.Request)
 	log.Printf("Se solicitará el chunk: %d ", req)
 	auxiliar := sendChunk((req), in.BookName)
 	return &pb.Test{Valor: in.Request, Chuck: auxiliar}, nil
 }
+/*-----------------------------------------------------------------------------------------*/
+
 
 /*-----------------------------------------------------------------------------------------*/
 func clientsReception() {
@@ -42,9 +46,10 @@ func clientsReception() {
 		log.Fatalf("failed to serve: %s", err)
 	}
 }
-
 /*-----------------------------------------------------------------------------------------*/
 
+
+/*-----------------------------------------------------------------------------------------*/
 func gutTheFile(FileName string) uint64 {
 	fileToBeChunked := FileName
 	file, err := os.Open(fileToBeChunked)
@@ -84,8 +89,10 @@ func gutTheFile(FileName string) uint64 {
 	}
 	return totalPartsNum
 }
+/*-----------------------------------------------------------------------------------------*/
 
-/**---------------------------------------------------------------------------------------------wwww*/
+
+/*-----------------------------------------------------------------------------------------*/
 func sendChunk(partToSend int, bookName string) []byte {
 	gutTheFile(bookName)
 	chunkToSend := bookName + "_" + strconv.FormatUint(uint64(partToSend), 10)
@@ -95,8 +102,45 @@ func sendChunk(partToSend int, bookName string) []byte {
 	}
 	return chunkBytes
 }
+/*-----------------------------------------------------------------------------------------*/
+
 
 /*-----------------------------------------------------------------------------------------*/
+func (s *Server) YadaYada(ctx context.Context, in *pb.Book) (*pb.Distribution, error) {
+	req := int(in.Request)
+	nm := in.BookName
+	auxiliar := createDistribution(req , fileName string)
+	return &pb.Distribution{Proposal: auxiliar}, nil
+}
+
+/*-----------------------------------------------------------------------------------------*/
+//gutsTheFile retorna el total de partes
+func createDistribution(numParts int, fileName string) []byte {
+	ax := ""
+	m := [4]string{"dist157", "dist158", "dist159", "dist160"}
+	strParts := strconv.Itoa(numParts)
+	c := numParts
+	e := 0
+	newDistr := fileName + " " + strParts + "\n"
+	for i := 0; i < 4; i++ {
+		ax = strconv.Itoa(i + 1)
+		newDistr = newDistr + "parte_1_" + ax + " " + m[i] + "\n"
+		c--
+		e ++
+	}
+	ind := 0
+	if c != 0 {
+		for j := c; j != 0; j-- {
+			ind ++
+			randomIndex := rand.Intn(len(m))
+			pick := m[randomIndex]
+			newDistr = newDistr + "parte_1_" + strconv.Itoa(e+ind) + " " + pick + "\n"
+		}
+	}
+	//fmt.Println(newDistr)
+	return []byte(newDistr)
+}
+
 
 func main() {
 	go clientsReception()
@@ -104,5 +148,10 @@ func main() {
 	for opcion != -1 {
 		fmt.Println("Ingrese -1 para cerrar el programa ")
 		fmt.Scanf("%d", &opcion)
+		if (opcion == 1){
+			createDistribution(5,"test")
+
+
+		}
 	}
 }
