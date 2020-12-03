a911586a4eae4a1c1d1e180e12d8d5d99a0ad490
jorge <jorge.diazma@sansano.usm.cl>
2020-12-02 23:19:31 -0300

    1

--- datanode/main.go
+++ datanode/main.go
@@ -37,6 +37,12 @@ func (s *Server) SayHello(ctx context.Context, in *pb.Book) (*pb.Test, error) {
 	auxiliar := sendChunk((req), in.BookName)
 	return &pb.Test{Valor: in.Request, Chuck: auxiliar}, nil
 }
+/*-----------------------------------------------------------------------------------------*/
+func (s *Server) TesteoEstado(ctx context.Context, in *pb.Bla) (*pb.Bla, error) {
+	req := int(in.Valor)
+	log.Printf("Se solicitará el chunk: %d ", req)
+	return &pb.Bla{Valor:1}, nil
+}
 
 /*-----------------------------------------------------------------------------------------*/
 func (s *Server) ClientToDataNode(ctx context.Context, in *pb.DataChuck) (*pb.Resultado, error) {
